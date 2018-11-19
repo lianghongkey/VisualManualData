3b35381656f4873d78aad9ef74ea39501f374dff
hong.liang <hong.liang@bitmain.com>
2018-11-19 15:09:20 +0800

    bug

--- model.py
+++ model.py
@@ -17,35 +17,22 @@ import numpy as np
 #         self.conv1 = nn.Conv2d(1,  8, kernel_size=5)
 #         self.conv2 = nn.Conv2d(8,  8, kernel_size=3)
 #         self.conv3 = nn.Conv2d(8, 2, kernel_size=5)
-#
 #     def forward(self, x):
-#
-#
 #         x = F.sigmoid(F.max_pool2d(self.conv1(x), 2))
 #         x = F.sigmoid(F.max_pool2d(self.conv2(x), 2))
 #         x = self.conv3(x)
-#
 #         x = x.view(-1, 1*2)
-#
 #         return F.log_softmax(x, dim=1)
 
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1,  2, kernel_size=3)
-        self.conv2 = nn.Conv2d(2,  2, kernel_size=3)
-        # self.conv3 = nn.Conv2d(2, 2, kernel_size=5)
-        self.fc1 = nn.Linear(26 * 26 * 2, 2)
+        self.conv1 = nn.Conv2d(1,  2, kernel_size=3 ,bias=False)
 
     def forward(self, x):
 
         x = F.sigmoid(self.conv1(x))
-
-
-        # x = x.view(-1,26*26*2)
-        # x = self.fc1(x)
-
         x = F.max_pool2d(x,26)
         x = x.view(-1, 2)
 
